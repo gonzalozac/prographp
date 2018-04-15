0912d759ef12213e8e006b94ac815550900a8569
gonzalozac <g_zacarias@hotmail.com>
2018-04-15 17:06:43 -0500

    agregoconecbd

--- funciones.php
+++ funciones.php
@@ -1,4 +1,17 @@
 <?php 
 echo "Hola mundo";
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 
 ?>
