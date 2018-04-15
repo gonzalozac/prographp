bd3ec8a6937174e4c020c40a55e158f1fcd3bd63
gonzalozac <g_zacarias@hotmail.com>
2018-04-15 17:12:19 -0500

    proceduresql

--- funciones.php
+++ funciones.php
@@ -14,4 +14,19 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
+
 ?>
